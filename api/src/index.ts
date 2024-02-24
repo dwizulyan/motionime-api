3aa7ec94095d83d58a38756b3391a532d0a03749
dwizulyan <dwizulyanhamid@gmail.com>
2024-02-25 06:13:13 +0700

    adding GET /api/merch route

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,4 +5,4 @@ const app = new Hono(); // init hono
 
 app.route("/api", api); // set the route for /api to be handled by the /api router
 
-export default app;
+export default app; // export the app
--- api/src/routes/api/index.ts
+++ api/src/routes/api/index.ts
@@ -2,11 +2,27 @@ import { Hono, Context } from "hono"; // import hono and type context
 
 const api = new Hono(); // initialize hono
 
-// create route /api/
+// create route /api/greetings
 api.get("/greetings", (c: Context) => {
   return c.json({
     success: true,
     message: "Helo Hono",
+  });
+});
+
+api.get("/merch", (c: Context) => {
+  const { category } = c.req.query();
+  let message: string;
+  if (category) {
+    message = `Getting merch with ${category} category`;
+  } else {
+    message = "Getting all merch";
+  }
+
+  return c.json({
+    success: true,
+    message: message,
+    data: {},
   });
 });
 
