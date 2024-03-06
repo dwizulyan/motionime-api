576a42152ea6ed4de6ea22572413426bbfa7b0f6
jeki <dwizulyanhamid@gmail.com>
2024-03-06 13:20:43 +0700

    adding category routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from "hono"; // import hono
 import api from "./routes/api/";
+import {logger} from "hono/logger";
 
 const app = new Hono(); // init hono
+
+app.use("*",logger());
 
 app.route("/api/merch", api.merch);
 app.route("/api/banner",api.banner);
 app.route("/api/event",api.event);
+app.route("/api/category",api.category);
 export default app; // export the app
--- api/src/routes/api/index.ts
+++ api/src/routes/api/index.ts
@@ -1,5 +1,6 @@
 import merch from "./merch";
 import banner from "./banner";
 import event from "./event";
+import category from "./category";
 
-export default {merch,banner,event};
+export default {merch,banner,event,category};
