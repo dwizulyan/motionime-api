c2dc44744605083bb5a4bc142707c02823c099e3
jeki <dwizulyanhamid@gmail.com>
2024-03-06 07:39:01 +0700

    implementing zod validator and bearer auth also removing test.rest file

--- api/src/routes/api/merch.ts
+++ api/src/routes/api/merch.ts
@@ -1,8 +1,31 @@
 import {Hono,Context} from "hono";
 import {PrismaClient} from "@prisma/client";
+import {zValidator} from "@hono/zod-validator";
+import {z} from "zod";
+import {bearerAuth} from "hono/bearer-auth"; 
 
 const merch = new Hono();
 const prisma = new PrismaClient();
+
+const token = Bun.env.token as string;
+
+const schema = z.object({
+    title : z.string(),
+    link:z.string(),
+    image:z.string(),
+    price:z.number(),
+    categoryId:z.number()
+})
+
+const zodHandler = zValidator("json",schema,(result,c)=>{
+    if(!result.success){
+        return c.json({
+            success:false,
+            message:"Invalid Data",
+            data : {},
+        },400)
+    }
+})
 
 merch.get("/",(c:Context)=>{
     return c.json({
@@ -11,4 +34,15 @@ merch.get("/",(c:Context)=>{
         data : {},
     })
 })
+
+merch.post("/create",zodHandler,bearerAuth({token}), async (c:Context)=>{
+    const data = await c.req.json();
+    return c.json({
+        success:true,
+        message:"Creating File",
+        data : data,
+    })
+})
+
+
 export default merch;
